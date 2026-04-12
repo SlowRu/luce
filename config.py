a9cb272965f830d90c67b1da2b68f2c2d6b8b86d
SlowRu <sanyasamson08@gmail.com>
2026-04-12 16:44:17 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "8680466852:AAGlGmoqRCFOjJsXxk6s7wNWfWV45ylyu3I"  # Вставь токен от @BotFather
+
+BOT_USERNAME = "@lucedbot"
+SITE_URL = "https://luced.ru"
+CONTACT_EMAIL = "mollyhuetonn@gmail.com"
