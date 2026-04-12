0b72217a383088514f656da782928f68157aac71
SlowRu <sanyasamson08@gmail.com>
2026-04-12 16:54:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ BOT_TOKEN = "8680466852:AAGlGmoqRCFOjJsXxk6s7wNWfWV45ylyu3I"  # Вставь т�
 BOT_USERNAME = "@lucedbot"
 SITE_URL = "https://luced.ru"
 CONTACT_EMAIL = "mollyhuetonn@gmail.com"
+
+# ID владельца (число). Чтобы узнать свой ID, напишите боту @userinfobot
+# Можно указать один или несколько через запятую
+ADMIN_IDS = [8207714217]  # пример: [123456789, 987654321]
