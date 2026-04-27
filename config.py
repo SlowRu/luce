2aa91295e8d0b0450631fe0d1c308baf307b0baf
SlowRu <sanyasamson08@gmail.com>
2026-04-27 20:10:48 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,21 @@
-BOT_TOKEN = "8680466852:AAGlGmoqRCFOjJsXxk6s7wNWfWV45ylyu3I"  # Вставь токен от @BotFather
+# config.py
+from dataclasses import dataclass
 
-BOT_USERNAME = "@lucedbot"
-SITE_URL = "https://luced.ru"
-CONTACT_EMAIL = "mollyhuetonn@gmail.com"
+@dataclass
+class Config:
+    BOT_TOKEN: str = "8680466852:AAGlGmoqRCFOjJsXxk6s7wNWfWV45ylyu3I"
+    
+    # TON Connect
+    TON_WALLET: str = "YOUR_TON_WALLET_ADDRESS"
+    TONCENTER_API_KEY: str = "YOUR_TONCENTER_API_KEY"
+    
+    # Настройки игр
+    MIN_BET_STARS: int = 1
+    MAX_BET_STARS: int = 1000
+    MIN_BET_TON: float = 0.1
+    MAX_BET_TON: float = 100.0
+    
+    # Комиссия казино (5%)
+    HOUSE_EDGE: float = 0.05
 
-# ID владельца (число). Чтобы узнать свой ID, напишите боту @userinfobot
-# Можно указать один или несколько через запятую
-ADMIN_IDS = [8207714217]  # пример: [123456789, 987654321]
+config = Config()
